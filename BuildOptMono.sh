f9c610b6d21bffaf5f5b71a188a0166737a96ac3
Neil Mayhew <neil_mayhew@sil.org>
2014-02-03 11:53:05 -0700

    Allow BuildOptMono.sh to be used from elsewhere in the filesystem
    Change-Id: Id7ee3bf5c8476d4d0cb58249bbfc2208f198f828

--- BuildOptMono.sh
+++ BuildOptMono.sh
@@ -2,11 +2,11 @@
 [ -z "$MONO_PREFIX" ] && export MONO_PREFIX=/opt/mono-sil
 # don't try bootstrapping the shell script wrapper if the build fails
 set -e
-./build-and-install
+"$(dirname $0)"/build-and-install
 # bootstrap the shell script wrapper as the mono that gets called (if needed)
 if [ ! -f $MONO_PREFIX/bin/mono-real ]; then
     sudo /bin/mv -v $MONO_PREFIX/bin/mono $MONO_PREFIX/bin/mono-real
-    sudo /bin/cp -v $(dirname $0)/mono-sil $MONO_PREFIX/bin/mono
+    sudo /bin/cp -v "$(dirname $0)"/mono-sil $MONO_PREFIX/bin/mono
     sudo /bin/chmod +x $MONO_PREFIX/bin/mono
 fi
 # show the user the current state of affairs (if everything succeeded)
