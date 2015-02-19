c8124430736607a33995052048f5efc5f2e96e99
Neil Mayhew <neil_mayhew@sil.org>
2015-02-18 17:44:05 -0700

    Make saving & restoring of mono wrapper more robust
     * Use a better test for when it needs to be done
     * Use trap to ensure temporary renaming is always undone
     * Clean out obsolete/buggy symlinks and wrappers
    
    Without these changes, building libgdiplus without mono (made possible
    in 1c27fe0) causes a circular loop of symlinks and running an app with
    the wrapper script will hang.
    
    Change-Id: I30b590b0c0fa010fc0139ab0fd2850164cb58dc5

--- BuildOptMono.sh
+++ BuildOptMono.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
+
 [ -z "$MONO_PREFIX" ] && export MONO_PREFIX=/opt/mono-sil
+
+MONO_BIN=$DESTDIR$MONO_PREFIX/bin
+
+# fix obsolete symlink
+if [ -L $MONO_BIN/mono -a "$(readlink $MONO_BIN/mono)" = $MONO_BIN/mono-sil ]; then
+    # make the symlink relative
+    sudo ln -sf mono-sil $MONO_BIN/mono
+fi
+
+# fix obsolete wrapper
+if [ -L $MONO_BIN/mono -a "$(readlink -f $MONO_BIN/mono)" = $MONO_BIN/mono-fw ]; then
+    # make a new wrapper after the build
+    sudo rm -f $MONO_BIN/mono-{fw,sil}
+    [ -e $MONO_BIN/mono-real ] && sudo mv -f $MONO_BIN/mono{-real,}
+fi
+
 # don't try bootstrapping the shell script wrapper if the build fails
 set -e
+
 "$(dirname $0)"/build-and-install "$@"
+
 # bootstrap the shell script wrapper as the mono that gets called (if needed)
-if [ ! -f $MONO_PREFIX/bin/mono-real ]; then
-    sudo /bin/mv -v $MONO_PREFIX/bin/mono $MONO_PREFIX/bin/mono-real
-    sudo /bin/cp -v "$(dirname $0)"/mono-sil $MONO_PREFIX/bin/mono-sil
-    sudo /bin/chmod +x $MONO_PREFIX/bin/mono-sil
-    sudo /bin/ln -s $MONO_PREFIX/bin/mono{-sil,}
+if [ ! -f $MONO_BIN/mono-sil ]; then
+    sudo cp -v "$(dirname $0)"/mono-sil $MONO_BIN/mono-sil
+    [ -e $MONO_BIN/mono ] && sudo mv -v $MONO_BIN/mono{,-real}
+    sudo ln -sf mono-sil $MONO_BIN/mono
 fi
+
 # show the user the current state of affairs (if everything succeeded)
-/bin/ls -l $MONO_PREFIX/bin/mono{,-fw,-real,-sil}
+ls -l $MONO_BIN/mono{,-sil,-real}
